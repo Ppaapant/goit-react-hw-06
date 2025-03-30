7d0f8b0e69613fdbae6da1da07227e9100d33a3e
Papant <andriy.chornenko@ukr.net>
2025-03-30 20:16:42 +0300

    add markup

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import contactsReducer from "./contactsSlice";
 import storage from "redux-persist/lib/storage";
 import filtersReducer from "./filtersSlice";
 import { persistStore, persistReducer } from "redux-persist";
-
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
 const contactsPersistConfig = {
 	key: "contacts",
@@ -21,7 +21,13 @@ export const store = configureStore({
     reducer: {
 		contacts: persistReducer(contactsPersistConfig, contactsReducer),
 		filters: persistReducer(filtersPersistConfig, filtersReducer),
-	   }
+	   },
+	   middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+		  serializableCheck: {
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+		  },
+		}),
 });
 
 export const persistor = persistStore(store);
